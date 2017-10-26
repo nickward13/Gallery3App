266983aa7972ba38f57df5988784e12879445b50
nickward13 <nickward@microsoft.com>
2017-10-27 09:44:29 +1100

    added azure mobile center analytics and crash analytics

--- GalleryApp/GalleryApp/App.xaml.cs
+++ GalleryApp/GalleryApp/App.xaml.cs
@@ -5,6 +5,10 @@ using System.Text;
 
 using Xamarin.Forms;
 
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+
 namespace GalleryApp
 {
 	public partial class App : Application
@@ -12,6 +16,10 @@ namespace GalleryApp
 		public App ()
 		{
 			InitializeComponent();
+
+            MobileCenter.Start("android=cad2b569-91fe-41f8-9652-deb6ddfb089f;" +
+                   "ios=9ee90fd3-6d44-4db1-be00-98614aa7051f;",
+                   typeof(Analytics), typeof(Crashes));
 
             MainPage = new NavigationPage(new AlbumPage(1));
 		}
